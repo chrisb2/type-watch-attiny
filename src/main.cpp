5b3d9c8ce6d69371971273371112814deeb7a856
Chris Borrill <chris@borrill.gen.nz>
2024-01-22 18:59:25 +1300

    Store threshold in EEPROM so it servives restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 #include <Bounce2.h>
 #include <ezLED.h>
+#include <EEPROM.h>
 
 // Threshold control values in millivolts
 const int minThreshold = 1200;
 const int maxThreshold = 2200;
 const int stepSize = 100;
-int threshold = 1700;
+int threshold = 0;
 
 // Pins
 const int piezoPin1 = A2;
 const int piezoPin2 = A3;
 const int buttonPin = 2;
+
+const int eepromAddr = 0;
 
 // On period for LED flash
 const int flashDelay = 200;
@@ -27,11 +30,14 @@ ezLED led(DISPLAY_LED);
 int readPiezo(int pin);
 void printMillivolts(int v1, int v2);
 void printThreshold(int threshold);
+int readThreshold();
+void writeThreshold(int currThreshold);
 
 void setup() {
   button.attach(buttonPin, INPUT_PULLUP);
   button.interval(5);
   led.blinkInPeriod(100, 100, 1000);
+  threshold = readThreshold();
 #ifdef UNO
   Serial.begin(9600);
   Serial.println("Begin");
@@ -50,6 +56,7 @@ void loop() {
     } else {
       led.blinkNumberOfTimes(100, 100, 1);
     }
+    writeThreshold(threshold);
 #ifdef UNO
     printThreshold(threshold);
 #endif
@@ -74,6 +81,16 @@ int readPiezo(int pin) {
   
   // convert the average analog reading to millivolts
   return (int)(average * mvFactor); 
+}
+
+int readThreshold() {
+  int val = EEPROM.read(eepromAddr);
+  return (val * stepSize) + minThreshold;
+}
+
+void writeThreshold(int currThreshold) {
+  int val = (currThreshold - minThreshold) / stepSize;
+  EEPROM.write(eepromAddr, val);
 }
 
 #ifdef UNO
